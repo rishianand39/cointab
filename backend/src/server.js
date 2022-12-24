c29c1b413f4c1cb32da8ad8aea9fd3c792d0dcd5
rishianand39 <rishi.rn818@gmail.com>
2022-12-24 21:25:03 +0530

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import app from "./index.js"
 import connect from "./configs/db.js";
+import dotenv from "dotenv";
+dotenv.config()
 
-
-const PORT = 8000;
+const PORT =process.env.PORT || 8001;
 app.listen(PORT,async()=>{
     try {
         await connect();
